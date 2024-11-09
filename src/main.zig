1b34b8c7131a82bc917e8fb38c9c3c7507084600
Filipp Druan <druanf@bk.ru>
2024-11-09 17:54:38 +0300

    Исправил программу

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,11 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    const num_1 = try allocator.alloc(LispObj, 1);
-    const num_2 = try allocator.alloc(LispObj, 1);
-    num_1.number = 32;
-    num_2.number = 64;
-    const cell = ConsCell.new(allocator, num_1, num_2);
+    const num_1 = try allocator.create(LispObj);
+    const num_2 = try allocator.create(LispObj);
+    num_1.* = LispObj{ .number = 32 };
+    num_2.* = LispObj{ .number = 32 };
+    const cell = try ConsCell.new(allocator, num_1, num_2);
     std.debug.print("car = {d}, cdr = {d}", .{ cell.car.number, cell.cdr.number });
 
     // stdout is for the actual output of your application, for example if you
@@ -35,7 +35,7 @@ const ConsCell = struct {
         car: *LispObj,
         cdr: *LispObj,
     ) !*ConsCell {
-        var cell = allocator.alloc(ConsCell, 1);
+        var cell = try allocator.create(ConsCell);
         cell.car = car;
         cell.cdr = cdr;
         return cell;
