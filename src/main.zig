ad5134abe2f68494c1604c4cf4a99342e09bf29c
Filipp Druan <druanf@bk.ru>
2024-12-19 13:59:58 +0300

    Удалил тело main. Я пишу тесты, она пока не нужна.

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,4 @@
 const std = @import("std");
 const berlisp = @import("berlisp.zig");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
-
-    const mem_man = berlisp.memory.MemoryManager.new(allocator);
-    _ = mem_man; // autofix
-
-}
+pub fn main() !void {}
