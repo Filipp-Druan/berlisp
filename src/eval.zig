3655dcf79c144d948bbd473647be1e086cfeb13b
Filipp Druan <druanf@bk.ru>
2025-02-27 12:10:43 +0300

    Начал писать функцию eval

--- src/eval.zig
+++ src/eval.zig
@@ -9,9 +9,37 @@ const bt = berlisp.base_types;
 
 const MemoryManager = mem.MemoryManager;
 const GCObj = mem.GCObj;
-const Enviroment = env.Environment;
+const Environment = env.Environment;
+
+const EvalError = error {
+    CalledNotAFunction,
+};
 
 /// TODO
-pub fn eval(mem_man: MemoryManager, code: *GCObj, env: Enviroment) !*GCObj {
-    switch (code.obj) {}
+pub fn eval(code: *GCObj, mem_man: *MemoryManager, current_env: *GCObj) !*GCObj {
+    switch (code.obj) {
+        .number => return code.take(mem_man),
+        .str => return code.take(mem_man),
+        .symbol => current_env.takeValBySym(code, mem_man),
+        .cons_cell => evalCall(code, mem_man, current_env),
+    }
 }
+
+pub fn evalCall(code: *GCObj, mem_man: *MemoryManager, current_env: *GCObj) !*GCObj {
+    const first = code.obj.cons_cell.car;
+    switch (first.obj) {
+        .symbol => {
+            switch (first) {
+                mem_man.special_symbols.quote_sym => {
+                    const
+                }
+            }
+        }
+    }
+}
+
+pub fn getOperator(code: *GCObj, mem_man: *MemoryManager, current_env: *GCObj) !*GCObj {
+
+}
+
+// Мне нужно написать функцию, которая принимает код, и исполняет - eval.
