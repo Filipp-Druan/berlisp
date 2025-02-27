bac25e9400440485f93e6b493f0994aaa57048d9
Filipp Druan <druanf@bk.ru>
2025-02-27 12:13:10 +0300

    Добавил файлы с eval и functions

--- src/berlisp.zig
+++ src/berlisp.zig
@@ -2,3 +2,5 @@ pub const base_types = @import("base_types.zig");
 pub const memory = @import("memory.zig");
 pub const env = @import("env.zig");
 pub const builder = @import("Builder.zig");
+pub const eval = @import("eval.zig");
+pub const functions = @import("functoins.zig");
