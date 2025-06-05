89e995370130912d734788c6790c4af9be5e652a
Filipp Druan <druanf@bk.ru>
2025-06-05 11:53:13 +0300

    Написал интерфейс интерпретатора. Он работает!

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,7 @@ const read_module = berlisp.read_module;
 
 const assert = std.debug.assert;
 
+const GCObj = berlisp.memory.GCObj;
 const Interpreter = berlisp.interpreter.Interpreter;
 
 pub fn main() !void {
@@ -17,7 +18,15 @@ pub fn main() !void {
     var interpreter = try Interpreter.init(allocator);
     defer interpreter.deinit();
 
-    _ = try interpreter.readEval("(foo (quote sym) (quote sym))");
+    var stdin = std.io.getStdIn().reader();
+    var buffer: [4096]u8 = undefined;
+
+    while (true) {
+        const line_len = try stdin.read(&buffer);
+        const expr = try interpreter.readEval(buffer[0..line_len]);
+        berlisp.printer.print(expr);
+        std.debug.print("\n", .{});
+    }
 }
 
 test "main test" {
